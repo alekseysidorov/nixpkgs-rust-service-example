7b7b98a575b9d9cadc6ccc52bff0c75b1983ae98
Aleksey Sidorov <sauron1987@gmail.com>
2023-02-23 13:31:04 +0400

    Update example

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -eo pipefail
 
-NIX_CROSS_SYSTEM=${NIX_CROSS_SYSTEM:-'{ config = "x86_64-unknown-linux-musl"; isStatic = true; useLLVM = true; }'}
+NIX_CROSS_SYSTEM=${NIX_CROSS_SYSTEM:-'{ config = "x86_64-unknown-linux-musl"; isStatic = false; useLLVM = true; }'}
 service_name=${1}
 
 # Compile service
