aa342cef9983173c54af52be917275bb50a4aaa2
Aleksey Sidorov <sauron1987@gmail.com>
2023-03-14 14:58:46 +0400

    Polish shell.nix file
    - Use `nativeBuildInputs` as runtime dependencies for the compiled Rust binary
    - Fix cargoDeps packages target
    - Force example service to use symbols from the compiled system libraries
    - Add code to check that the HTTPS is working fine

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -5,5 +5,5 @@ NIX_CROSS_SYSTEM=${NIX_CROSS_SYSTEM:-'{ config = "x86_64-unknown-linux-musl"; is
 
 # Compile service
 nix-shell --pure --arg crossSystem "${NIX_CROSS_SYSTEM}" --run "cargo build --release"
-image_archive=$(nix-build ./shell.nix -A dockerImage --arg crossSystem "$NIX_CROSS_SYSTEM")
+image_archive=$(nix-build ./shell.nix --no-out-link -A dockerImage --arg crossSystem "$NIX_CROSS_SYSTEM")
 echo $image_archive
