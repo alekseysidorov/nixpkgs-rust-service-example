3614692aa795ccac5f55b99a2418a70ab123ff76
Aleksey Sidorov <sauron1987@gmail.com>
2023-10-21 11:10:11 +0300

    Migrate project to a modern flake-parts approach

--- flake.nix
+++ flake.nix
@@ -1,26 +1,56 @@
 {
   inputs = {
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-cross-overlay = {
       url = "github:alekseysidorov/nixpkgs-cross-overlay";
       inputs = {
-        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
       };
     };
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
   };
 
-  outputs = { flake-utils, ... }: { } // flake-utils.lib.eachDefaultSystem
-    (localSystem:
-      {
-        devShells = {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.flake-root.flakeModule
+      ];
+
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        devShells = let localSystem = system; in {
           default = import ./shell.nix { inherit localSystem; };
           cross = import ./shell.nix {
             inherit localSystem;
             crossSystem = { config = "x86_64-unknown-linux-musl"; useLLVM = true; };
           };
         };
-      }
-    );
+
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+
+          programs.nixpkgs-fmt.enable = true;
+          programs.rustfmt.enable = true;
+          programs.beautysh.enable = true;
+          programs.deno.enable = true;
+          programs.taplo.enable = true;
+        };
+
+        formatter = config.treefmt.build.wrapper;
+      };
+    };
 }
